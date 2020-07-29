3d61d6339b6e995745920ef2394ccc9bbb3885ee
Alex <24409942+inckie@users.noreply.github.com>
2020-07-29 12:37:12 -0400

    + UI even list filter

--- plugins/quick-brick-xray/android/src/main/java/com/applicaster/plugin/xray/ui/EventLogFragment.kt
+++ plugins/quick-brick-xray/android/src/main/java/com/applicaster/plugin/xray/ui/EventLogFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.applicaster.plugin.xray.R
 import com.applicaster.plugin.xray.XRayPlugin
 import com.applicaster.xray.core.Core
+import com.applicaster.xray.core.LogLevel
 import com.applicaster.xray.example.sinks.InMemoryLogSink
 
 /**
@@ -20,21 +24,37 @@ class EventLogFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
         val view = inflater.inflate(R.layout.fragment_event_log_list, container, false)
 
         // We expect our example Plugin to provide this sink as InMemoryLogSink
         val inMemoryLogSink = Core.get().getSink(XRayPlugin.inMemorySinkName) as InMemoryLogSink?
 
         // todo: show message if sink is missing
+        if (null != inMemoryLogSink) {
+            val list = view.findViewById<RecyclerView>(R.id.list)
 
-        // Set the adapter
-        if (view is RecyclerView && null != inMemoryLogSink) {
-            with(view) {
-                adapter = EventRecyclerViewAdapter(
-                        viewLifecycleOwner,
-                        inMemoryLogSink.getLiveData()
-                )
+            // Wrap original list to filtered one
+            val filteredList = FilteredEventList(viewLifecycleOwner, inMemoryLogSink.getLiveData())
+
+            // Setup log level filter spinner
+            val levels = view.findViewById<Spinner>(R.id.cb_filter)
+            levels.adapter = ArrayAdapter(levels.context, android.R.layout.simple_list_item_1, LogLevel.values())
+            levels.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    filteredList.filter = LogLevel.values()[position]
+                }
             }
+
+            // Setup the list adapter
+            list.adapter = EventRecyclerViewAdapter(
+                    viewLifecycleOwner,
+                    filteredList
+            )
+
         }
         return view
     }
