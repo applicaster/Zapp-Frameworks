2fa8e9a9bab59a8fde04183031e99f47911ec79b
kononenkoAnton <a.kononenko@applicaster.com>
2020-09-03 14:44:35 -0400

    Add new version for xray iOS (#54)

--- plugins/zapp-apple/apple/universal/Helpers/UrlScheme/UrlSchemeHandler.swift
+++ plugins/zapp-apple/apple/universal/Helpers/UrlScheme/UrlSchemeHandler.swift
@@ -8,32 +8,43 @@
 import Foundation
 
 public class UrlSchemeHandler {
-    
     enum SystemActions: String {
         case generateNewUUID
         case plugin
+        case xray
     }
-    
+
     public class func handle(with rootViewController: RootController?,
                              application: UIApplication,
                              open url: URL,
                              options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        
         var retValue = false
-        //create action with string
+        // create action with string
         let action = SystemActions(rawValue: url.host ?? "")
-        
-        //check if action defined anc call its implementation or skip
+
+        // check if action defined anc call its implementation or skip
         switch action {
         case .generateNewUUID:
-            retValue = self.handleUUIDregeneration(with: rootViewController)
-            // This section is experimental, not in use. If not needed in future, please remove it, with handlePluginURLScheme: func
+            retValue = handleUUIDregeneration(with: rootViewController)
+        case .xray:
+            retValue = handleXray(url: url,
+                                  rootViewController: rootViewController)
         case .plugin:
+            // This section is experimental, not in use. If not needed in future, please remove it, with handlePluginURLScheme: func
             retValue = handlePluginURLScheme(url: url)
         default:
             break
         }
-        
-       return retValue
+
+        return retValue
+    }
+
+    class func queryParams(url: URL) -> [String: Any]? {
+        guard let components = URLComponents(url: url,
+                                             resolvingAgainstBaseURL: true),
+            let queryItems = components.queryItems else { return nil }
+        return queryItems.reduce(into: [String: String]()) { result, item in
+            result[item.name] = item.value
+        }
     }
 }
