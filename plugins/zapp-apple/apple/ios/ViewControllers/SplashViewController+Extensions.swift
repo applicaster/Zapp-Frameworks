504ece3c0dca7b442f946929e858f5af7f255484
kononenkoAnton <a.kononenko@applicaster.com>
2020-10-21 11:30:33 -0400

    Add launch screen orientation (#98)

---
+++ plugins/zapp-apple/apple/ios/ViewControllers/SplashViewController+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewController+Extensions.swift
+//  ZappApple
+//
+//  Created by Anton Kononenko on 10/19/20.
+//  Copyright © 2020 Applicaster LTD. All rights reserved.
+//
+
+import UIKit
+
+extension SplashViewController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIDevice.current.userInterfaceIdiom == .pad ? .landscape : .portrait
+    }
+}
