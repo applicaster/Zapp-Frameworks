792038cfdb104e714879c4d6bfc719e9c0813248
Alex Zchut <a.zchut@applicaster.com>
2020-11-03 19:47:52 +0200

    update param

--- plugins/quick-brick-apple-user-activity/src/AppleUserActivity.ts
+++ plugins/quick-brick-apple-user-activity/src/AppleUserActivity.ts
@@ -1,8 +1,8 @@
 import { defineUserActivity } from "./bridge";
 
 export function run(payload, callback) {
-  if (payload?.extensions?.contentId) {
-    defineUserActivity({ contentId: payload?.extensions?.contentId });
+  if (payload?.extensions?.apple_umc_show_content_id) {
+    defineUserActivity({ apple_umc_show_content_id: payload?.extensions?.apple_umc_show_content_id });
   }
 
   callback({ success: true, payload });
