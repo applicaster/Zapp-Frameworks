4fbd680e5096287a3439dc74ee71132468b51218
Alex Zchut <a.zchut@applicaster.com>
2020-11-22 12:25:26 +0200

    feat: Add Adobe analytics plugin

---
+++ plugins/zapp-analytics-plugin-adobe/apple/ios/ZappAnalyticsPluginAdobe.h
@@ -0,0 +1,19 @@
+//
+//  ZappAnalyticsPluginAdobe.h
+//  ZappAnalyticsPluginAdobe
+//
+//  Created by Alex Zchut on 22/11/2020.
+//  Copyright © 2020 Applicaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZappAnalyticsPluginAdobe.
+FOUNDATION_EXPORT double ZappAnalyticsPluginAdobeVersionNumber;
+
+//! Project version string for ZappAnalyticsPluginAdobe.
+FOUNDATION_EXPORT const unsigned char ZappAnalyticsPluginAdobeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZappAnalyticsPluginAdobe/PublicHeader.h>
+
+#import <ZappAnalyticsPluginAdobe/AdobeAnalytics.h>
