8f57620a0be7a372f47d482d4fc34f097c0eb62f
Alex <24409942+inckie@users.noreply.github.com>
2020-12-01 17:06:17 -0500

    Data class tweak

--- plugins/quick-brick-xray/android/src/main/java/com/applicaster/plugin/xray/remote/EventBatch.kt
+++ plugins/quick-brick-xray/android/src/main/java/com/applicaster/plugin/xray/remote/EventBatch.kt
@@ -1,6 +1,8 @@
 package com.applicaster.plugin.xray.remote
 
-class LogBatch(val device_id: String,
-               val entries: List<String>)
+data class LogBatch(
+        val device_id: String,
+        val entries: List<String>
+)
 
-class LogPack(val log_batch: LogBatch)
+data class LogPack(val log_batch: LogBatch)
