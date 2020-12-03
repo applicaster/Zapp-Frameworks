aaebd0454bf80944201e2469c1566f1765034ee4
kononenkoAnton <a.kononenko@applicaster.com>
2020-12-02 20:30:09 -0500

    update

--- plugins/video_playing_now_apple/apple/tvos/ZPAppleVideoNowPlayingInfo+PlayerObserverProtocol.swift
+++ plugins/video_playing_now_apple/apple/tvos/ZPAppleVideoNowPlayingInfo+PlayerObserverProtocol.swift
@@ -121,7 +121,6 @@ extension ZPAppleVideoNowPlayingInfo {
             metadataItems.append(playbackProgresItem)
 
             currentItem.externalMetadata = metadataItems
-            print("testAnton \(currentProgress) - \(metadataItems)")
         }
     }
 
