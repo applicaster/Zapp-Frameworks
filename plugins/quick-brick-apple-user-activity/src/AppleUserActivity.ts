1e7fbd3484b1d1e0f721c456f3e908a3fa7ed093
Alex Zchut <a.zchut@applicaster.com>
2020-12-08 14:27:12 +0200

    add activity removal on player close

--- plugins/quick-brick-apple-user-activity/src/AppleUserActivity.ts
+++ plugins/quick-brick-apple-user-activity/src/AppleUserActivity.ts
@@ -1,9 +1,17 @@
-import { defineUserActivity } from "./bridge";
-
+import { playerManager } from "@applicaster/zapp-react-native-utils/appUtils/playerManager";
+​
+import { defineUserActivity, removeUserActivity } from "./bridge";
+​
+function cleanUp() {
+  removeUserActivity();
+  playerManager.removeHandler("close", cleanUp);
+};
+​
 export function run(payload, callback) {
   if (payload?.extensions?.apple_umc_show_content_id) {
     defineUserActivity({ apple_umc_show_content_id: payload?.extensions?.apple_umc_show_content_id });
+​
+    playerManager.on("close", cleanUp);
   }
-
+​
   callback({ success: true, payload });
-}
