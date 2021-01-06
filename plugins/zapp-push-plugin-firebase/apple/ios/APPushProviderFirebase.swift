92cdc2aebc718befed14a6172a62d158f9591ff2
Alex Zchut <a.zchut@applicaster.com>
2021-01-06 19:03:44 +0200

    update to topics

--- plugins/zapp-push-plugin-firebase/apple/ios/APPushProviderFirebase.swift
+++ plugins/zapp-push-plugin-firebase/apple/ios/APPushProviderFirebase.swift
@@ -18,7 +18,7 @@ open class APPushProviderFirebase: ZPPushProvider {
     lazy var logger = Logger.getLogger(for: "\(kNativeSubsystemPath)/ZappPushPluginFirebase")
     let localStorageTopicsParam = "topics"
 
-    var registeredTags: Set<String> = []
+    var registeredTopics: Set<String> = []
     override open func getKey() -> String {
         return model?.identifier ?? "ZappPushPluginFirebase"
     }
@@ -44,7 +44,7 @@ open class APPushProviderFirebase: ZPPushProvider {
 
     override open func disable(completion: ((Bool) -> Void)?) {
         logger?.debugLog(message: "Handle disable plugin logic")
-        removeTagsToDevice(Array(registeredTags)) { [weak self] _, _ in
+        removeTagsToDevice(Array(registeredTopics)) { [weak self] _, _ in
             guard let `self` = self else { return }
             self.unsubscribeUUID()
             Messaging.messaging().delegate = nil
@@ -54,85 +54,63 @@ open class APPushProviderFirebase: ZPPushProvider {
         }
     }
 
-    open func subscribeToTopic(with topic: String, completion: ((_ success: Bool) -> Void)?) {
-        Messaging.messaging().subscribe(toTopic: topic) { error in
-            if error == nil {
-                self.logger?.debugLog(message: "Successfully subscribed to topic: \(topic)")
-                completion?(true)
-            } else {
-                self.logger?.debugLog(message: "Failed to subscribe to topic: \(topic)")
-                completion?(false)
-            }
+    open func addTagsToDevice(_ tags: [String]?,
+                              completion: @escaping (_ success: Bool, _ tags: [String]?) -> Void) {
+        guard let topics = tags else {
+            completion(true, nil)
+            return
         }
-    }
 
-    func subscribeToTopics(with topics: [String]) {
+        logger?.debugLog(message: "Subscribe to topics",
+                         data: ["topics": topics])
+
+        let dispatchGroup = DispatchGroup()
         for topic in topics {
-            subscribeToTopic(with: topic, completion: nil)
-        }
-    }
-
-    open func unsubscribeFromTopic(with topic: String, completion: ((_ success: Bool) -> Void)?) {
-        Messaging.messaging().unsubscribe(fromTopic: topic) { error in
-            if error == nil {
-                self.logger?.debugLog(message: "Successfully unsubscribed from topic: \(topic)")
-                completion?(true)
-            } else {
-                self.logger?.debugLog(message: "Failed to unsubscribe from topic: \(topic)")
-                completion?(false)
-            }
-        }
-    }
-
-    open func addTagsToDevice(_ tags: [String]?, completion: @escaping (_ success: Bool, _ tags: [String]?) -> Void) {
-        guard let tags = tags else {
-            completion(true, nil)
-            return
-        }
-
-        logger?.debugLog(message: "Add tags to device",
-                         data: ["tags": tags])
-
-        var success = true
-        let dispatchGroup = DispatchGroup()
-        for tag in tags {
             dispatchGroup.enter()
-            Messaging.messaging().subscribe(toTopic: tag) { [weak self] error in
-                guard let `self` = self else { return }
-                success = success && (error == nil)
-                self.registeredTags.insert(tag)
+            Messaging.messaging().subscribe(toTopic: topic) { error in
+                if error == nil {
+                    self.registeredTopics.insert(topic)
+                }
                 dispatchGroup.leave()
             }
         }
 
         dispatchGroup.notify(queue: .main) {
-            completion(success, tags)
+            self.logger?.debugLog(message: "Currently subscribed topics updated",
+                                  data: ["topics": Array(self.registeredTopics)])
+
+            self.updateTopicsInLocalStorage()
+            completion(true, topics)
         }
     }
 
-    open func removeTagsToDevice(_ tags: [String]?, completion: @escaping (_ success: Bool, _ tags: [String]?) -> Void) {
-        guard let tags = tags else {
+    open func removeTagsToDevice(_ tags: [String]?,
+                                 completion: @escaping (_ success: Bool, _ tags: [String]?) -> Void) {
+        guard let topics = tags else {
             completion(true, nil)
             return
         }
 
-        logger?.debugLog(message: "Remove tags to device",
-                         data: ["tags": tags])
+        logger?.debugLog(message: "Unsubscribe from topics",
+                         data: ["topics": topics])
 
-        var success = true
         let dispatchGroup = DispatchGroup()
-        for tag in tags {
+        for topic in topics {
             dispatchGroup.enter()
-            Messaging.messaging().unsubscribe(fromTopic: tag) { [weak self] error in
-                guard let `self` = self else { return }
-                self.registeredTags.remove(tag)
-                success = success && (error == nil)
+            Messaging.messaging().unsubscribe(fromTopic: topic) { error in
+                if error == nil {
+                    self.registeredTopics.remove(topic)
+                }
                 dispatchGroup.leave()
             }
         }
 
         dispatchGroup.notify(queue: .main) {
-            completion(success, tags)
+            self.logger?.debugLog(message: "Currently subscribed topics updated",
+                                  data: ["topics": Array(self.registeredTopics)])
+
+            self.updateTopicsInLocalStorage()
+            completion(true, topics)
         }
     }
 
@@ -160,15 +138,15 @@ open class APPushProviderFirebase: ZPPushProvider {
     }
 
     fileprivate func setDefaultTopicIfNeeded() {
-        if let _ = activeTopics {
+        if let _ = registeredTopicsInLocalStorage {
             // topics already defined, no changes needed
         } else {
             // add default value
             if let defaultTopic = configurationJSON?["default_topic"] as? String,
                defaultTopic.isEmpty == false {
-                _ = FacadeConnector.connector?.storage?.localStorageSetValue(for: localStorageTopicsParam,
-                                                                             value: defaultTopic,
-                                                                             namespace: namespace)
+                addTagsToDevice([defaultTopic]) { _, _ in
+                    // do nothing
+                }
             }
         }
     }
@@ -177,13 +155,20 @@ open class APPushProviderFirebase: ZPPushProvider {
         return model?.identifier
     }
 
-    fileprivate var activeTopics: [String]? {
+    fileprivate var registeredTopicsInLocalStorage: [String]? {
         let topicsString = FacadeConnector.connector?.storage?.localStorageValue(for: localStorageTopicsParam,
                                                                                  namespace: namespace)
 
         return topicsString?.components(separatedBy: ",")
             .map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
             .filter { $0.isEmpty == false }
+    }
+
+    fileprivate func updateTopicsInLocalStorage() {
+        let topics = Array(registeredTopics).joined(separator: ",")
+        _ = FacadeConnector.connector?.storage?.localStorageSetValue(for: localStorageTopicsParam,
+                                                                     value: topics,
+                                                                     namespace: namespace)
     }
 }
 
@@ -197,7 +182,11 @@ extension APPushProviderFirebase: MessagingDelegate {
         logger?.debugLog(message: "didReceiveRegistrationToken: token:\(fcmToken)",
                          data: ["token": fcmToken])
 
-        // subscribe to active topics
-        subscribeToTopics(with: activeTopics ?? [])
+        // clean local registered topics
+        registeredTopics.removeAll()
+        // subscribe to topics appears in local storage
+        addTagsToDevice(registeredTopicsInLocalStorage, completion: { _, _ in
+            // do nothing
+        })
     }
 }
