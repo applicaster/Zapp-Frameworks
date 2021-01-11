6b39d711c9d86ac95b79f20e9cd2c811da9250ab
kononenkoAnton <a.kononenko@applicaster.com>
2021-01-11 13:34:34 -0500

    update

--- plugins/zapp-react-native-theo-player/manifests/manifest.config.js
+++ plugins/zapp-react-native-theo-player/manifests/manifest.config.js
@@ -16,21 +16,19 @@ const baseManifest = {
 };
 
 function createManifest({ version, platform }) {
-  const basePlatform = platform.includes("ios") ? "ios" : "android";
-
   const manifest = {
     ...baseManifest,
 
     platform,
     dependency_version: version,
     manifest_version: version,
-    api: api[basePlatform],
-    npm_dependencies: npm_dependencies[basePlatform],
-    dependency_repository_url: dependency_repository_url[basePlatform],
+    api: api[platform],
+    npm_dependencies: npm_dependencies[platform],
+    dependency_repository_url: dependency_repository_url[platform],
     min_zapp_sdk: min_zapp_sdk[platform],
-    extra_dependencies: extra_dependencies[basePlatform],
-    project_dependencies: project_dependencies[basePlatform],
-    custom_configuration_fields: custom_configuration_fields[basePlatform],
+    extra_dependencies: extra_dependencies[platform],
+    project_dependencies: project_dependencies[platform],
+    custom_configuration_fields: custom_configuration_fields[platform],
   };
 
   return manifest;
