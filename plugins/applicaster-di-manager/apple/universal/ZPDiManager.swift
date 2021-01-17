a6d35649bb253eccb0046943ece603af695cf72a
Alex Zchut <alexzchut@users.noreply.github.com>
2021-01-17 12:10:24 +0200

    feat: Adding di manager plugin (#166)
    * feat: Add DI server configuration

---
+++ plugins/applicaster-di-manager/apple/universal/ZPDiManager.swift
@@ -0,0 +1,113 @@
+//
+//  ZPDiManager.swift
+//  ZappDiManager
+//
+//  Created by Alex Zchut on 14/01/2021.
+//  Copyright © 2021 Applicaster Ltd. All rights reserved.
+//
+
+import AdSupport
+import XrayLogger
+import ZappCore
+
+@objc public class ZPDiManager: NSObject, GeneralProviderProtocol {
+    public var model: ZPPluginModel?
+    public var configurationJSON: NSDictionary?
+    lazy var logger = Logger.getLogger(for: "\(kNativeSubsystemPath)/ZappDiManager")
+
+    enum DiError: String, Error {
+        case NoDataReturnedFromServer = "Error: no data"
+        case JsonParsingFailed = "Error: conversion from JSON failed"
+        case NoJWTvalue = "Error: JWT param can not be fetched"
+    }
+
+    struct Params {
+        static let jwtJsonKey = "jwt"
+        static let jwtStorageKey = "signedDeviceInfoToken"
+    }
+
+    public required init(pluginModel: ZPPluginModel) {
+        model = pluginModel
+        configurationJSON = model?.configurationJSON
+    }
+
+    public var providerName: String {
+        return String(describing: Self.self)
+    }
+
+    public func prepareProvider(_ defaultParams: [String: Any],
+                                completion: ((_ isReady: Bool) -> Void)?) {
+        makeDiServerCall(completion)
+    }
+
+    public func disable(completion: ((Bool) -> Void)?) {
+        _ = FacadeConnector.connector?.storage?.localStorageRemoveValue(for: Params.jwtStorageKey,
+                                                                        namespace: nil)
+        completion?(true)
+    }
+
+    public func makeDiServerCall(_ completion: ((_ success: Bool) -> Void)?) {
+        guard let urlString = configurationJSON?["di_server_url"] as? String,
+              let url = URL(string: urlString) else {
+            logger?.errorLog(message: "Server Url not defined")
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            do {
+                guard let data = data else {
+                    throw DiError.NoDataReturnedFromServer
+                }
+
+                guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    throw DiError.JsonParsingFailed
+                }
+
+                guard let jwt = json[Params.jwtJsonKey] as? String else {
+                    throw DiError.NoJWTvalue
+                }
+                _ = FacadeConnector.connector?.storage?.localStorageSetValue(for: Params.jwtStorageKey,
+                                                                             value: jwt,
+                                                                             namespace: nil)
+
+            } catch let error as DiError {
+                self.logger?.errorLog(message: "DI Server error",
+                                      data: ["url": urlString,
+                                             "error": error.localizedDescription])
+            } catch let error as NSError {
+                self.logger?.errorLog(message: "DI Server error",
+                                      data: ["url": urlString,
+                                             "error": error.localizedDescription])
+            }
+
+            if self.shouldWaitForCompletion {
+                completion?(true)
+            }
+
+        }.resume()
+
+        if shouldWaitForCompletion == false {
+            completion?(true)
+        }
+    }
+
+    lazy var shouldWaitForCompletion: Bool = {
+        var retValue: Bool = true
+
+        guard let value = configurationJSON?["wait_for_completion"] else {
+            return retValue
+        }
+
+        // Check if value bool or string
+        if let stringValue = value as? String {
+            if let boolValue = Bool(stringValue) {
+                retValue = boolValue
+            } else if let intValue = Int(stringValue) {
+                retValue = Bool(truncating: intValue as NSNumber)
+            }
+        } else if let boolValue = value as? Bool {
+            retValue = boolValue
+        }
+        return retValue
+    }()
+}
---
+++ plugins/applicaster-di-manager/manifests/manifest.config.js
@@ -0,0 +1,106 @@
+const baseManifest = {
+  api: {},
+  dependency_repository_url: [],
+  author_name: "Applicaster",
+  author_email: "zapp@applicaster.com",
+  name: "Di Manager",
+  description: "Call Di and store response to session storage",
+  type: "general",
+  screen: false,
+  react_native: false,
+  ui_builder_support: true,
+  whitelisted_account_ids: [],
+  ui_frameworks: ["quickbrick"],
+  core_plugin: true,
+  min_zapp_sdk: "1.0.0",
+  deprecated_since_zapp_sdk: "",
+  unsupported_since_zapp_sdk: "",
+  custom_configuration_fields: [],
+  identifier: "di_manager_call",
+  npm_dependencies: [],
+  targets: ["mobile"],
+};
+
+function createManifest({ version, platform }) {
+  const manifest = {
+    ...baseManifest,
+    platform,
+    manifest_version: version,
+    min_zapp_sdk: min_zapp_sdk[platform],
+    extra_dependencies: extra_dependencies[platform],
+    api: api[platform],
+    npm_dependencies: [`@applicaster/applicaster-di-manager@${version}`],
+    targets: targets[platform],
+    ui_frameworks: ui_frameworks[platform]
+  };
+  return manifest;
+}
+
+const custom_configuration_fields_apple = [
+  {
+    key: "enabled",
+    type: "checkbox",
+    default: 1,
+    label: "Plugin enabled",
+    label_tooltip:
+      "Disable plugin if you do not want to use plugin",
+  },
+  {
+    key: "wait_for_completion",
+    type: "checkbox",
+    default: 1,
+    label: "Block UI until completed",
+    label_tooltip:
+      "Disable if you dont want to block UI until process completions",
+  },
+  {
+    key: "di_server_url",
+    type: "text",
+    label: "Di Server url",
+    default: "https://di.applicaster.com",
+    tooltip_text: "Set Di Server Url"
+  }
+];
+
+const min_zapp_sdk = {
+  ios_for_quickbrick: "3.0.0-Dev",
+  tvos_for_quickbrick: "3.0.0-Dev",
+};
+
+const extra_dependencies_apple = {
+  ZappDiManager:
+    ":path => './node_modules/@applicaster/applicaster-di-manager/apple/ZappDiManager.podspec'",
+};
+
+const ui_frameworks_qb = ["quickbrick"];
+const ui_frameworks_native = ["native"];
+const ui_frameworks = {
+  ios: ui_frameworks_native,
+  ios_for_quickbrick: ui_frameworks_qb,
+  tvos_for_quickbrick: ui_frameworks_qb,
+};
+
+const extra_dependencies = {
+  ios_for_quickbrick: [extra_dependencies_apple],
+  tvos_for_quickbrick: [extra_dependencies_apple],
+};
+
+const api_apple = {
+  require_startup_execution: false,
+  class_name: "ZPDiManager",
+  modules: ["ZappDiManager"],
+};
+
+const api = {
+  ios_for_quickbrick: api_apple,
+  tvos_for_quickbrick: api_apple,
+};
+
+const mobileTarget = ["mobile"];
+const tvTarget = ["tv"];
+const targets = {
+  ios_for_quickbrick: mobileTarget,
+  tvos_for_quickbrick: tvTarget,
+};
+
+module.exports = createManifest;
