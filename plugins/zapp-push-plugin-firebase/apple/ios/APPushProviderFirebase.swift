75fb394e3ac8a50ac8c335134596bae44c351105
Alex Zchut <a.zchut@applicaster.com>
2021-01-20 21:47:47 +0200

    update firebase back to 6.25.0

--- plugins/zapp-push-plugin-firebase/apple/ios/APPushProviderFirebase.swift
+++ plugins/zapp-push-plugin-firebase/apple/ios/APPushProviderFirebase.swift
@@ -173,9 +173,8 @@ open class APPushProviderFirebase: ZPPushProvider {
 }
 
 extension APPushProviderFirebase: MessagingDelegate {
-    open func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        guard let fcmToken = fcmToken,
-              fcmToken.isEmpty == false else {
+    open func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        guard !fcmToken.isEmpty else {
             logger?.debugLog(message: "didReceiveRegistrationToken: token is empty")
             return
         }
