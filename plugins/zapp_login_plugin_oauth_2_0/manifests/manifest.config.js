ebab916014fbe4151645ef704490da0c62740701
kononenkoAnton <a.kononenko@applicaster.com>
2021-01-25 13:41:19 -0500

    update

--- plugins/zapp_login_plugin_oauth_2_0/manifests/manifest.config.js
+++ plugins/zapp_login_plugin_oauth_2_0/manifests/manifest.config.js
@@ -48,7 +48,7 @@ const baseManifest = {
           conditional_fields: [
             {
               condition_value: ["aws_cognito", "custom"],
-              key: "data/provider_selector",
+              key: "custom_configuration_fields/provider_selector",
             },
           ],
         },
@@ -61,7 +61,7 @@ const baseManifest = {
           conditional_fields: [
             {
               condition_value: ["aws_cognito"],
-              key: "data/provider_selector",
+              key: "custom_configuration_fields/provider_selector",
             },
           ],
         },
