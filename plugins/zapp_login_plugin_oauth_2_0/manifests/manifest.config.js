c23767433f6b923a9b322d2ddd5bce824e31923e
kononenkoAnton <a.kononenko@applicaster.com>
2021-01-25 18:19:23 -0500

    Update

--- plugins/zapp_login_plugin_oauth_2_0/manifests/manifest.config.js
+++ plugins/zapp_login_plugin_oauth_2_0/manifests/manifest.config.js
@@ -163,7 +163,7 @@ const baseManifest = {
           label: "Dangerously Allow Insecure Http Requests",
           tooltip_text:
             "ANDROID whether to allow requests over plain HTTP or with self-signed SSL certificates. ⚠️ Can be useful for testing against local server, should not be used in production. This setting has no effect on iOS; to enable insecure HTTP requests, add a NSExceptionAllowsInsecureHTTPLoads exception to your App Transport Security settings.",
-          initial_value: false,
+          default: false,
         },
         {
           type: "switch",
@@ -171,7 +171,7 @@ const baseManifest = {
           label: "Use Nonce",
           tooltip_text:
             "IOS (default: true) optionally allows not sending the nonce parameter, to support non-compliant providers",
-          initial_value: true,
+          default: true,
         },
         {
           type: "switch",
@@ -179,7 +179,7 @@ const baseManifest = {
           label: "Use PKCE",
           tooltip_text:
             "(default: true) optionally allows not sending the code_challenge parameter and skipping PKCE code verification, to support non-compliant providers.",
-          initial_value: true,
+          default: true,
         },
         {
           type: "switch",
@@ -187,7 +187,7 @@ const baseManifest = {
           label: "Skip Code Exchange",
           tooltip_text:
             "(default: false) just return the authorization response, instead of automatically exchanging the authorization code. This is useful if this exchange needs to be done manually (not client-side)",
-          initial_value: false,
+          default: false,
         },
       ],
     },
