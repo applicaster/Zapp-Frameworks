c97883aad6c965e7fabaa5d47c78f9c2ae2ef4e3
Alex <24409942+inckie@users.noreply.github.com>
2021-02-08 18:07:05 -0500

    Fix fullscreen

--- plugins/zapp-react-native-theo-player/android/src/main/java/com/theoplayerreactnative/TheoPlayerViewManager.java
+++ plugins/zapp-react-native-theo-player/android/src/main/java/com/theoplayerreactnative/TheoPlayerViewManager.java
@@ -1,6 +1,9 @@
 package com.theoplayerreactnative;
 
+import android.app.Activity;
+import android.os.Build;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
@@ -144,6 +147,8 @@ public class TheoPlayerViewManager extends SimpleViewManager<THEOplayerView> imp
         addPropertyChangeListeners(reactContext);
         reactContext.addLifecycleEventListener(this);
 
+        goFullscreen();
+
         return playerView;
     }
 
@@ -207,4 +212,12 @@ public class TheoPlayerViewManager extends SimpleViewManager<THEOplayerView> imp
         playerView.onDestroy();
     }
 
+    private void goFullscreen() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 }
