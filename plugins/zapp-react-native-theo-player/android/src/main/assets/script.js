f626758ed4acb4650a7c4709211ee865685e63d1
Alex <24409942+inckie@users.noreply.github.com>
2021-02-10 17:53:28 -0500

    - copy js/css for android

---
+++ plugins/zapp-react-native-theo-player/android/src/main/assets/script.js
@@ -0,0 +1,27 @@
+function init({ player })
+{
+    if (player) {
+        // setting up the forward button by setting up a video-js component
+        var Button = THEOplayer.videojs.getComponent('Button');
+        var CloseButton = THEOplayer.videojs.extend(Button, {
+            constructor: function() {
+                Button.apply(this, arguments);
+                /* initialize your button */
+            },
+            handleClick: () => {
+            var event = {"type" : "onCloseButtonHandle"}
+                if(!window?.webkit) {
+                              theoplayerAndroid.sendMessage("onJSMessageReceived", JSON.stringify(event));
+                            } else {
+                              window.webkit.messageHandlers.onJSMessageReceived.postMessage(event);
+                            }
+            },
+            buildCSSClass: function() {
+                return 'vjs-icon-close'; // insert all class names here
+            }
+        });
+
+        THEOplayer.videojs.registerComponent('CloseButton', CloseButton);
+        player.ui.getChild('TopControlBar').addChild('CloseButton', {});
+    }
+}
