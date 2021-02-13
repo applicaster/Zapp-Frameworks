76620c4f29ea4f10d8a80c2137630e6860b0404a
Alex Zchut <a.zchut@applicaster.com>
2021-02-13 22:34:39 +0200

    fix for ima player observer crash

--- plugins/quick-brick-google-ima-apple/apple/universal/GoogleInteractiveMediaAdsAdapter/GoogleInteractiveMediaAdsAdapter+QBPlayerObserverProtocol.swift
+++ plugins/quick-brick-google-ima-apple/apple/universal/GoogleInteractiveMediaAdsAdapter/GoogleInteractiveMediaAdsAdapter+QBPlayerObserverProtocol.swift
@@ -57,7 +57,6 @@ extension GoogleInteractiveMediaAdsAdapter: PlayerObserverProtocol {
     }
 
     public func playerDidDismiss(player: PlayerProtocol) {
-        avPlayer?.removeObserver(self, forKeyPath: MediaAdsConstants.playerPlaybackRate)
         playerPlugin = nil
         adsLoader?.delegate = nil
         adsLoader = nil
