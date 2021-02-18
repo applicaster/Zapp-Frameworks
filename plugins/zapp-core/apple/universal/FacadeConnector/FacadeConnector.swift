f85a91bbc033b611d7f67d6f0099f08c98c6790f
Alex Zchut <alexzchut@users.noreply.github.com>
2021-02-18 07:26:08 +0200

    adding analytics back to connector as deprecated (#195)

--- plugins/zapp-core/apple/universal/FacadeConnector/FacadeConnector.swift
+++ plugins/zapp-core/apple/universal/FacadeConnector/FacadeConnector.swift
@@ -18,6 +18,11 @@ import UIKit
         return connectorProvider as? FacadeConnectorAppDataProtocol
     }
 
+    @available(*, deprecated, message: "Deprecated from QB SDK 5.0.0, use EventsBus instead")
+    @objc public var analytics: FacadeConnectorAnalyticsProtocol? {
+        return connectorProvider as? FacadeConnectorAnalyticsProtocol
+    }
+
     @objc public var playerDependant: FacadeConnectorPlayerDependantProtocol? {
         return connectorProvider as? FacadeConnectorPlayerDependantProtocol
     }
