786f289e604cca7e2de63ad6c362c429dbe58e84
Alex Zchut <alexzchut@users.noreply.github.com>
2021-02-18 07:36:16 +0200

    feat: Adobe Analytics - add support for tvOS (#194)
    * feat: Adobe Analytics - add support for tvOS
    
    * update for manifest
    
    * update

--- plugins/zapp-analytics-plugin-adobe/apple/universal/APAnalyticsProviderAdobe+AdobeAnalyticsDelegate.swift
+++ plugins/zapp-analytics-plugin-adobe/apple/universal/APAnalyticsProviderAdobe+AdobeAnalyticsDelegate.swift
@@ -5,8 +5,8 @@
 //  Created by Alex Zchut on 22/11/2020.
 //
 
-import Foundation
 import AVFoundation
+import Foundation
 import ZappCore
 
 extension APAnalyticsProviderAdobe: AdobeAnalyticsDelegate {
@@ -14,21 +14,19 @@ extension APAnalyticsProviderAdobe: AdobeAnalyticsDelegate {
         guard let value = FacadeConnector.connector?.storage?.sessionStorageValue(for: "application_environment", namespace: nil) else {
             return false
         }
-        
+
         return Bool(value) ?? false
     }
-    
+
     public func getDeviceID() -> String! {
         return FacadeConnector.connector?.storage?.sessionStorageValue(for: "uuid", namespace: nil) ?? ""
     }
-    
-    public func getCurrentPlayedItemEntry() -> [AnyHashable : Any] {
-        return self.playerPlugin?.entry ?? [:]
+
+    public func getCurrentPlayedItemEntry() -> [AnyHashable: Any] {
+        return playerPlugin?.entry ?? [:]
     }
-    
+
     public func getCurrentPlayerInstance() -> AVPlayer? {
-        return self.avPlayer
+        return avPlayer
     }
-    
-    
 }
---
+++ plugins/zapp-analytics-plugin-adobe/apple/universal/APAnalyticsProviderAdobe+EventsBusTopics.swift
@@ -0,0 +1,27 @@
+//
+//  APAnalyticsProviderAdobe+HandleEventsBusTopics.swift
+//  ZappAnalyticsPluginAdobe
+//
+//  Created by Alex Zchut on 17/02/2021.
+//
+
+import Foundation
+import ZappCore
+
+extension APAnalyticsProviderAdobe {
+    func subscribeToEventsBusTopics() {
+        EventsBus.subscribe(self, name: "videoAdvertisementsOpportunity") { notification in
+            self.adobeAnalyticsObjc?.prepareVideoAdvertisementsOpportunity(notification, completion: { (eventName, parameters) in
+                let trackParameters = parameters as? [String: NSObject] ?? [:]
+                self.trackEvent(eventName, parameters: trackParameters)
+            })
+        }
+
+        EventsBus.subscribe(self, name: "watchVideoAdvertisements") { notification in
+            self.adobeAnalyticsObjc?.prepareWatchVideoAdvertisement(notification, completion: { (eventName, parameters) in
+                let trackParameters = parameters as? [String: NSObject] ?? [:]
+                self.trackEvent(eventName, parameters: trackParameters)
+            })
+        }
+    }
+}
