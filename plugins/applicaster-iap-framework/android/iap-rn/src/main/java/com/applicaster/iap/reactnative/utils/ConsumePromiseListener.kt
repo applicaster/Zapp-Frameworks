9ffc14c31697f8964eac88c7730d6d28f0046a7a
Alex <24409942+inckie@users.noreply.github.com>
2021-02-26 10:33:40 -0500

    feat: add logs and proguard rules to in-app purchasees (#202)
    * + add logs and proguard rules
    
    * fix gradle file

--- plugins/applicaster-iap-framework/android/iap-rn/src/main/java/com/applicaster/iap/reactnative/utils/ConsumePromiseListener.kt
+++ plugins/applicaster-iap-framework/android/iap-rn/src/main/java/com/applicaster/iap/reactnative/utils/ConsumePromiseListener.kt
@@ -1,13 +1,13 @@
 package com.applicaster.iap.reactnative.utils
 
-import android.util.Log
 import com.applicaster.iap.reactnative.IAPBridge
+import com.applicaster.util.APLogger
 import com.facebook.react.bridge.Promise
 
 class ConsumePromiseListener(promise: Promise) : PromiseListener(promise) {
 
     override fun onPurchaseConsumed(purchaseToken: String) {
-        Log.d(IAPBridge.TAG, "Purchases was consumed.")
+        APLogger.debug(IAPBridge.TAG, "Purchases was consumed.")
         promise.resolve(purchaseToken)
     }
 }
