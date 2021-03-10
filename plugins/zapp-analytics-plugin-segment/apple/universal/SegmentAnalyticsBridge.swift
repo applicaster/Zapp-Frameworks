41684ccc4a3bd4e9532b1b53e02c11582f9b0fef
Alex Zchut <alexzchut@users.noreply.github.com>
2021-03-10 15:58:42 +0200

    feat: Update Segment Analytics plugin to support sdk 1.0.0 (#213)
    * feat: Update Segment Analytics plugin to support sdk 1.0.0
    
    * update for audio language and subtitle change
    
    * updates

---
+++ plugins/zapp-analytics-plugin-segment/apple/universal/SegmentAnalyticsBridge.swift
@@ -0,0 +1,42 @@
+//
+//  SegmentAnalyticsBridge.swift
+//  SegmentAnalytics
+//
+//  Created by Alex Zchut on 10/03/2021.
+//
+
+import React
+import ZappCore
+
+@objc(SegmentAPI)
+class SegmentAnalyticsBridge: NSObject, RCTBridgeModule {
+    fileprivate let pluginIdentifier = "segment_analytics"
+    var bridge: RCTBridge!
+
+    static func moduleName() -> String! {
+        return "SegmentAPI"
+    }
+
+    public class func requiresMainQueueSetup() -> Bool {
+        return true
+    }
+
+    /// prefered thread on which to run this native module
+    @objc public var methodQueue: DispatchQueue {
+        return DispatchQueue.main
+    }
+
+    @objc public func identifyUser(_ userID: String,
+                                   traits: NSDictionary?,
+                                   options: NSDictionary?) {
+        guard let provider = FacadeConnector.connector?.pluginManager?.getProviderInstance(identifier: pluginIdentifier) as? SegmentAnalytics,
+              let traits = traits as? [String: Any],
+              let options = options as? [String: Any] else {
+            return
+        }
+
+        provider.login(with: userID,
+                       traits: traits,
+                       options: options)
+    }
+}
