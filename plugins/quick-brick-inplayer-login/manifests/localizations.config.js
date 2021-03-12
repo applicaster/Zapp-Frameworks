c2f680052a1d7f64cb3ecfeeaa4d21edf0f1881d
kononenkoAnton <a.kononenko@applicaster.com>
2021-03-12 10:40:16 -0500

    update manifest for login

--- plugins/quick-brick-inplayer-login/manifests/localizations.config.js
+++ plugins/quick-brick-inplayer-login/manifests/localizations.config.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 const common = [
   // Fields
   {
@@ -206,86 +204,9 @@ const mobile = [
     label: "Set new password button title",
     initial_value: "SET NEW PASSWORD",
   },
-
-  // StoreFront screen
-  {
-    key: "payment_screen_title_text",
-    label: "Payment screen title",
-    initial_value: "Choose Your Subscription",
-  },
-  {
-    key: "restore_purchases_text",
-    label: "Restore purchases description",
-    initial_value: "Purchased already a subscription?",
-  },
-  {
-    key: "restore_purchases_link",
-    label: "Restore purchases text",
-    initial_value: "Restore",
-  },
-  {
-    key: "terms_of_use_instructions_text",
-    label: "Payment terms of use instruction",
-    initial_value:
-      "By making a selection and completing this transaction, you verify that you are at least 18 years old and agree to the",
-  },
-  {
-    key: "terms_of_use_link_text",
-    label: "Payment terms of use text",
-    initial_value: "terms of use.",
-  },
 ];
 
 const tv = [
-  // Subscriber agreement screen
-  {
-    key: "privacy_main_title_text",
-    label: "Agreement and privacy screen title",
-    initial_value: "Subscriber Agreement & Privacy Policy",
-  },
-  {
-    key: "privacy_text",
-    label: "Agreement and privacy text",
-    initial_value:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet nunc dui. Sed nec dignissim erat. Praesent molestie, odio et lacinia dapibus, lacus felis interdum justo, a viverra eros mauris vel nibh. Nullam consequat urna at lorem interdum, non mattis elit interdum. Cras libero erat, mattis ut mattis in, ornare ut ante. Duis id mi condimentum elit sagittis scelerisque. Duis facilisis vel lectus eu fermentum. Etiam venenatis fermentum felis nec ornare. Nullam pretium iaculis ligula sed accumsan.\n\n\nDonec id libero sit amet ligula cursus tempor. Donec urna felis, vestibulum id fringilla in, elementum ac diam. Nunc pretium, ligula ac accumsan accumsan, ipsum ante tristique nisi, et dictum dui arcu eu ex. Duis vel lectus quis nisl fringilla dictum. Praesent vulputate justo ligula, at commodo lorem sodales sed. Cras quis rhoncus ante. Nunc ultricies orci eget purus elementum, eget posuere elit semper. Suspendisse quis dignissim elit, eget dictum sem. Sed in nisl dui. Curabitur at sapien consectetur, lacinia turpis vitae, pharetra nisl. Nullam accumsan odio orci, quis elementum ex luctus id. Cras nec eros orci. Vestibulum eget convallis lectus. Donec eu lorem at purus elementum tempo.",
-  },
-
-  // StoreFront screen
-  {
-    key: "subscription_default_title_text",
-    label: "Subscription default title",
-    initial_value: "Choose Your Subscription",
-  },
-  {
-    key: "policy_agreement_text",
-    label: "Policy agreement text",
-    initial_value:
-      "By clicking “Subscribe” or “Buy” below, you also agree to the [Client’s App Name] Agreement and acknowledge that you have read our Privacy Policy",
-  },
-  {
-    key: "subscriber_agreement_and_privacy_policy_text",
-    label: "Subscriber Agreement and Privacy policy text",
-    initial_value:
-      "[Client’s Name App] Subscriber Agreement and Privacy Policy",
-  },
-
-  // Action buttons
-  {
-    key: "restore_purchase_action_button_text",
-    label: "Restore purchases button title",
-    initial_value: "Restore",
-  },
-  {
-    key: "payment_option_action_text_type_buy",
-    label: "Buy button title",
-    initial_value: "Buy",
-  },
-  {
-    key: "payment_option_action_text_type_subscribe",
-    label: "Subscribe button title",
-    initial_value: "Subscribe",
-  },
-
   // Account screen
   {
     key: "login_title_text",
--- plugins/quick-brick-inplayer-login/src/Utils/Customization/index.js
+++ plugins/quick-brick-inplayer-login/src/Utils/Customization/index.js
@@ -3,19 +3,25 @@ import * as R from "ramda";
 import { platformSelect } from "@applicaster/zapp-react-native-utils/reactUtils";
 import { populateConfigurationValues } from "@applicaster/zapp-react-native-utils/stylesUtils";
 
-const manifestJson = platformSelect({
-  ios: require("../../../manifests/ios_for_quickbrick.json"),
-  tvos: require("../../../manifests/tvos_for_quickbrick.json"),
-  android: require("../../../manifests/android.json"),
-  android_tv: require("../../../manifests/android_tv_for_quickbrick.json"),
-  web: require("../../../manifests/samsung_tv.json"),
-  samsung_tv: require("../../../manifests/samsung_tv.json"),
-  // lg_tv: require("../../../manifests/lg_tv.json"),
-  default: require("../../../manifests/android.json"),
-});
+const manifestJson = () => {
+  try {
+    return platformSelect({
+      ios: require("../../../manifests/ios_for_quickbrick.json"),
+      tvos: require("../../../manifests/tvos_for_quickbrick.json"),
+      android: require("../../../manifests/android.json"),
+      android_tv: require("../../../manifests/android_tv_for_quickbrick.json"),
+      web: require("../../../manifests/samsung_tv.json"),
+      samsung_tv: require("../../../manifests/samsung_tv.json"),
+      lg_tv: require("../../../manifests/lg_tv.json"),
+      default: require("../../../manifests/android.json"),
+    });
+  } catch (error) {
+    throw new Error("Could not load manifest at inplayer login plugin.", error);
+  }
+};
 
 export function pluginIdentifier() {
-  return manifestJson.identifier;
+  return manifestJson().identifier;
 }
 
 export let styles = null;
@@ -24,7 +30,7 @@ export function getStyles(screenStyles) {
 }
 
 export function prepareStyles(screenStyles) {
-  styles = populateConfigurationValues(manifestJson.styles.fields)(
+  styles = populateConfigurationValues(manifestJson().styles.fields)(
     screenStyles
   );
   styles.import_parent_lock = screenStyles.import_parent_lock
