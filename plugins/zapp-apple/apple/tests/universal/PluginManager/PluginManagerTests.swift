c7bd4ff2bf43e6d2389723a60fb4e67785cef3bd
Alex Zchut <a.zchut@applicaster.com>
2021-03-15 13:13:11 +0200

    Revert "failure test"
    This reverts commit 376669b81d8f34225c9bf730cca58c796c7f5c36.

--- plugins/zapp-apple/apple/tests/universal/PluginManager/PluginManagerTests.swift
+++ plugins/zapp-apple/apple/tests/universal/PluginManager/PluginManagerTests.swift
@@ -72,7 +72,7 @@ extension PluginManagerTests {
 
         pluginManager.loadPluginsGroup {
             self.pluginModels = PluginsManager.parseLatestPluginsJson()
-            XCTAssertEqual(self.pluginModels?.count ?? 0, 0, ErrorMessages.pluginsListEmpty)
+            XCTAssertNotEqual(self.pluginModels?.count ?? 0, 0, ErrorMessages.pluginsListEmpty)
             expectation.fulfill()
         } _: {
             XCTAssert(true, ErrorMessages.pluginsListUnableToLoad)
