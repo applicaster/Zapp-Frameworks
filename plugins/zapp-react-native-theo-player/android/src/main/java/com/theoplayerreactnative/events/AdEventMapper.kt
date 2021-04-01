c3964006f79ac37751386fa6955ea8054d559ca5
Alex <24409942+inckie@users.noreply.github.com>
2021-04-01 16:02:54 -0400

    + more data in Ads events

--- plugins/zapp-react-native-theo-player/android/src/main/java/com/theoplayerreactnative/events/AdEventMapper.kt
+++ plugins/zapp-react-native-theo-player/android/src/main/java/com/theoplayerreactnative/events/AdEventMapper.kt
@@ -2,33 +2,59 @@ package com.theoplayerreactnative.events
 
 import com.facebook.react.bridge.Arguments.createMap
 import com.facebook.react.bridge.WritableMap
+import com.theoplayer.android.api.ads.*
 import com.theoplayer.android.api.event.ads.*
 
 object AdEventMapper {
 
     @JvmStatic
     fun toRN(event: AdBreakBeginEvent): WritableMap = createMap().apply {
-        putInt("timeOffset", event.adBreak.timeOffset)
+        collectAdBreakInfo(this, event.adBreak)
     }
 
     @JvmStatic
     fun toRN(event: AdBreakEndEvent): WritableMap = createMap().apply {
-        putInt("timeOffset", event.adBreak.timeOffset)
+        collectAdBreakInfo(this, event.adBreak)
+    }
+
+    private fun collectAdBreakInfo(map: WritableMap, adBreak: AdBreak) {
+        map.putInt("timeOffset", adBreak.timeOffset)
+        map.putInt("maxDuration", adBreak.maxDuration)
+        map.putDouble("maxRemainingDuration", adBreak.maxRemainingDuration)
+        map.putString("integrationKind", adBreak.integration.type)
     }
 
     @JvmStatic
     fun toRN(event: AdBeginEvent): WritableMap = createMap().apply {
-        event.ad?.let { putString("id", it.id) }
+        collectAdInfo(this, event.ad)
     }
 
     @JvmStatic
     fun toRN(event: AdEndEvent): WritableMap = createMap().apply {
-        event.ad?.let { putString("id", it.id) }
+        collectAdInfo(this, event.ad)
     }
 
     @JvmStatic
     fun toRN(event: AdErrorEvent): WritableMap = createMap().apply {
-        event.ad?.let { putString("id", it.id) }
+        collectAdInfo(this, event.ad)
         putString("error", event.error)
     }
+
+    private fun collectAdInfo(map: WritableMap, ad: Ad?) {
+        if(null == ad)
+            return
+        map.putString("id", ad.id)
+        map.putString("type", ad.type)
+        (ad as? LinearAd?)?.let {
+            map.putInt("duration", it.duration)
+        }
+        (ad as? GoogleImaAd?)?.let {
+            map.putString("creativeId", it.creativeId)
+            map.putString("adSystem", it.adSystem)
+        }
+        (ad as? NonLinearAd?)?.let {
+            map.putString("resourceURI", it.resourceURI)
+        }
+    }
+
 }
