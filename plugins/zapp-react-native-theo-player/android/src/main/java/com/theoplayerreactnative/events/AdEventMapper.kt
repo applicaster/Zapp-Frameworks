1054914b54d10b29f7d5980cd6939086e957088c
Alex <24409942+inckie@users.noreply.github.com>
2021-04-06 16:12:38 -0400

    ad breakSize

--- plugins/zapp-react-native-theo-player/android/src/main/java/com/theoplayerreactnative/events/AdEventMapper.kt
+++ plugins/zapp-react-native-theo-player/android/src/main/java/com/theoplayerreactnative/events/AdEventMapper.kt
@@ -22,6 +22,7 @@ object AdEventMapper {
         map.putInt("maxDuration", adBreak.maxDuration)
         map.putDouble("maxRemainingDuration", adBreak.maxRemainingDuration)
         map.putString("integrationKind", adBreak.integration.type)
+        map.putInt("breakSize", adBreak.ads.size)
     }
 
     @JvmStatic
@@ -55,6 +56,7 @@ object AdEventMapper {
         (ad as? NonLinearAd?)?.let {
             map.putString("resourceURI", it.resourceURI)
         }
+        ad.adBreak?.let { map.putInt("breakSize", it.ads.size) }
     }
 
 }
