37bdded398602b5e79a54d5eff5b595b694005aa
Alex <24409942+inckie@users.noreply.github.com>
2021-04-19 16:44:57 -0400

    + untested empty Home View

--- plugins/quick-brick-opta-stats/android/src/main/java/com/applicaster/opta/statsscreenplugin/reactnative/OptaStatsView.kt
+++ plugins/quick-brick-opta-stats/android/src/main/java/com/applicaster/opta/statsscreenplugin/reactnative/OptaStatsView.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import com.applicaster.opta.statsscreenplugin.R
+import com.applicaster.util.APLogger
 
 class OptaStatsView(context: Context) : FrameLayout(context) {
 
     init {
+        APLogger.info(TAG, "OptaStatsView created")
         LayoutInflater.from(context).inflate(R.layout.fragment_home, this, true)
     }
 
+    companion object {
+        private const val TAG = "OptaStatsView"
+    }
 }
