557978620c14c2e463237d803cb79c3066ef43a0
Alex <24409942+inckie@users.noreply.github.com>
2021-04-19 17:19:08 -0400

    * testing Home View

--- plugins/quick-brick-opta-stats/android/src/main/java/com/applicaster/opta/statsscreenplugin/reactnative/OptaStatsView.kt
+++ plugins/quick-brick-opta-stats/android/src/main/java/com/applicaster/opta/statsscreenplugin/reactnative/OptaStatsView.kt
@@ -1,16 +1,179 @@
 package com.applicaster.opta.statsscreenplugin.reactnative
 
 import android.content.Context
+import android.os.Build
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.applicaster.lesscodeutils.date.DateUtils
 import com.applicaster.opta.statsscreenplugin.R
+import com.applicaster.opta.statsscreenplugin.data.model.AllMatchesModel
+import com.applicaster.opta.statsscreenplugin.data.model.GroupModel
+import com.applicaster.opta.statsscreenplugin.data.model.MatchModel
+import com.applicaster.opta.statsscreenplugin.screens.home.HomeInteractor
+import com.applicaster.opta.statsscreenplugin.screens.home.HomePresenter
+import com.applicaster.opta.statsscreenplugin.screens.home.HomeView
+import com.applicaster.opta.statsscreenplugin.screens.home.adapters.GroupAdapter
+import com.applicaster.opta.statsscreenplugin.screens.home.adapters.MatchAdapter
+import com.applicaster.opta.statsscreenplugin.screens.match.MatchInteractor
+import com.applicaster.opta.statsscreenplugin.screens.match.MatchPresenter
+import com.applicaster.opta.statsscreenplugin.screens.match.MatchView
+import com.applicaster.opta.statsscreenplugin.utils.Constants
+import com.applicaster.opta.statsscreenplugin.utils.ModelUtils
+import com.applicaster.opta.statsscreenplugin.utils.PluginUtils
 import com.applicaster.util.APLogger
 
-class OptaStatsView(context: Context) : FrameLayout(context) {
+class OptaStatsView(context: Context) : FrameLayout(context),
+        HomeView, MatchView,
+        GroupAdapter.OnTeamFlagClickListener,
+        MatchAdapter.OnMatchClickListener {
 
     init {
         APLogger.info(TAG, "OptaStatsView created")
         LayoutInflater.from(context).inflate(R.layout.fragment_home, this, true)
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        pageIndicator = findViewById(R.id.pageIndicator)
+        rv_group_cards = findViewById(R.id.rv_group_cards)
+        rv_matches = findViewById(R.id.rv_matches)
+        pb_loading = findViewById(R.id.pb_loading)
+
+        homePresenter.getAllMatchesFromDate()
+        homePresenter.getGroups()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        homePresenter.onDestroy()
+        matchPresenter.onDestroy()
+    }
+
+    // instead of generics for now
+    private lateinit var pageIndicator: com.chahinem.pageindicator.PageIndicator
+    private lateinit var rv_group_cards: RecyclerView
+    private lateinit var rv_matches: RecyclerView
+    private lateinit var pb_loading: View
+
+    // todo: this should be configurable
+    lateinit var startDate: String
+    lateinit var endDate: String
+
+    // presenters
+    private var homePresenter: HomePresenter = HomePresenter(this, HomeInteractor())
+    private var matchPresenter: MatchPresenter = MatchPresenter(this, MatchInteractor())
+
+    // array to save matches from a specific date
+    private lateinit var matchesFromDate: List<AllMatchesModel.Match>
+
+    // above matches but with more details
+    private var matchesDetailed: ArrayList<MatchModel.Match> = ArrayList()
+
+    // counter to get the details from each match retrieved in a for loop
+    private var matchesDetailedCounter: Int = 0
+    // total of matches that we want to get the details
+    private var matchesDetailedTotal: Int = 0
+
+    fun heartbeat() {
+        // every time the heartbeat is called is needed to clear the matches
+        resetList()
+        homePresenter.getAllMatchesFromDate()
+    }
+
+    private fun resetList() {
+        matchesFromDate = ArrayList()
+        matchesDetailed = ArrayList()
+        matchesDetailedCounter = 0
+        matchesDetailedTotal = 0
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    override fun getGroupsSuccess(groupCards: GroupModel.Group) {
+        rv_group_cards.layoutManager = LinearLayoutManager(context)
+        rv_group_cards.isNestedScrollingEnabled = false
+        rv_group_cards.adapter = GroupAdapter(ModelUtils.getDivisionWithTypeTotal(groupCards), context, this)
+    }
+
+    override fun getGroupsFail(error: String?) {
+        Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun getAllMatchesFromDateSuccess(allMatchesFromDate: AllMatchesModel.AllMatches) {
+        startDate = allMatchesFromDate.tournamentCalendar.startDate
+        endDate = allMatchesFromDate.tournamentCalendar.endDate
+
+        val date = if (startDate > DateUtils.getCurrentDate(Constants.UTC_DATE_FORMAT)) startDate else DateUtils.getCurrentDate(Constants.UTC_DATE_FORMAT)
+        getAllMatchesDetailed(allMatchesFromDate, date)
+    }
+
+    private fun getAllMatchesDetailed(allMatches: AllMatchesModel.AllMatches, date: String) {
+        // this is the specific method that get all the matches from a specific date
+        matchesFromDate = ModelUtils.getNextThreeMatches(allMatches, date)
+        // check if there are matches for this day
+        if (matchesFromDate.isNotEmpty()) {
+            // initialize a counter that will be use to count each match details
+            matchesDetailedTotal = matchesFromDate.size
+            getMatchDetails(matchesDetailedCounter)
+        } else if (date > endDate) {
+            matchesFromDate = ModelUtils.getLastThreeMatches(allMatches)
+            matchesDetailedTotal = matchesFromDate.size
+            getMatchDetails(matchesDetailedCounter)
+        }
+
+    }
+
+    private fun getMatchDetails(matchesDetailedCounter: Int) {
+        matchPresenter.getMatchDetails(matchesFromDate[matchesDetailedCounter].id)
+    }
+
+    override fun getMatchSuccess(matches: MatchModel.Match) {
+        matchesDetailed.add(matches)
+        matchesDetailedCounter++
+        if (matchesDetailedCounter < matchesDetailedTotal) {
+            getMatchDetails(matchesDetailedCounter)
+        } else {
+            Log.d(TAG, "Details from each match have been retrieved")
+            matchesDetailedCounter = 0
+            matchesDetailedTotal = 0
+
+            // add extra element to build the extra card (to show all matches card)
+            matchesDetailed.add(MatchModel.Match())
+            // set up recycler view
+            rv_matches.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            rv_matches.adapter = MatchAdapter(matchesDetailed, context, this, this, false)
+            pageIndicator.attachTo(rv_matches)
+        }
+    }
+
+    override fun onTeamFlagClicked(teamId: String) {
+        PluginUtils.goToTeamScreen(teamId)
+    }
+
+    override fun onMatchClicked(matchId: String) {
+        PluginUtils.goToMatchDetailsScreen(matchId)
+    }
+
+    override fun getMatchFailed(error: String?) {
+        Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showProgress() {
+        pb_loading.visibility = View.VISIBLE
+    }
+
+    override fun hideProgress() {
+        pb_loading.visibility = View.GONE
     }
 
     companion object {
