3f5d79ca46a7fa629fc665a6461c9608dd844e13
Alex <24409942+inckie@users.noreply.github.com>
2021-04-19 18:34:24 -0400

    ! fix RN layout bug

--- plugins/quick-brick-opta-stats/android/src/main/java/com/applicaster/opta/statsscreenplugin/reactnative/OptaStatsView.kt
+++ plugins/quick-brick-opta-stats/android/src/main/java/com/applicaster/opta/statsscreenplugin/reactnative/OptaStatsView.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewTreeObserver
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -34,6 +35,8 @@ class OptaStatsView(context: Context) : FrameLayout(context),
         GroupAdapter.OnTeamFlagClickListener,
         MatchAdapter.OnMatchClickListener {
 
+    // todo: timers (handle focus lost as well)
+
     // instead of synthetics for now
     private var pageIndicator: com.chahinem.pageindicator.PageIndicator
     private var rv_group_cards: RecyclerView
@@ -58,6 +61,21 @@ class OptaStatsView(context: Context) : FrameLayout(context),
 
         homePresenter.getAllMatchesFromDate()
         homePresenter.getGroups()
+    }
+
+    override fun requestLayout() {
+        super.requestLayout()
+
+        // This view relies on a measure + layout pass happening after it calls requestLayout().
+        // https://github.com/facebook/react-native/issues/4990#issuecomment-180415510
+        // https://stackoverflow.com/questions/39836356/react-native-resize-custom-ui-component
+        post(measureAndLayout)
+    }
+
+    private val measureAndLayout = Runnable {
+        measure(MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY))
+        layout(left, top, right, bottom)
     }
 
     override fun onDetachedFromWindow() {
@@ -130,7 +148,6 @@ class OptaStatsView(context: Context) : FrameLayout(context),
             matchesDetailedTotal = matchesFromDate.size
             getMatchDetails(matchesDetailedCounter)
         }
-
     }
 
     private fun getMatchDetails(matchesDetailedCounter: Int) {
