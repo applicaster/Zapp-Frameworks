e063d670af5543f75cd39aa625c7b1b5f8734105
Alex <24409942+inckie@users.noreply.github.com>
2021-04-20 10:25:47 -0400

    unused import

--- plugins/quick-brick-opta-stats/android/src/main/java/com/applicaster/opta/statsscreenplugin/reactnative/OptaStatsView.kt
+++ plugins/quick-brick-opta-stats/android/src/main/java/com/applicaster/opta/statsscreenplugin/reactnative/OptaStatsView.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewTreeObserver
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.annotation.RequiresApi
