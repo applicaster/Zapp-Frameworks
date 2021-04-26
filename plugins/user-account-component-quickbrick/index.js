0a00c842034caaae8fb700d162f65825fd4fbc88
kononenkoAnton <a.kononenko@applicaster.com>
2021-04-26 12:33:33 -0400

    FEAT: User Account

---
+++ plugins/user-account-component-quickbrick/index.js
@@ -0,0 +1,6 @@
+import { withNavigator } from "@applicaster/zapp-react-native-ui-components/Decorators/Navigator/";
+import AccountComponent from "./src/Screens/AccountComponent";
+
+const UserAccount = withNavigator(AccountComponent);
+
+export default UserAccount;
