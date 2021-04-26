57adf7e741c1f0aec2e869134380f52d62916d04
kononenkoAnton <a.kononenko@applicaster.com>
2021-04-26 13:07:53 -0400

    update

--- plugins/user-account-component-quickbrick/src/Styles/DefaultStyles.js
+++ plugins/user-account-component-quickbrick/src/Styles/DefaultStyles.js
@@ -2,12 +2,12 @@ export const fontsize = {
   account_component_instructions: 60,
   account_component_greetings: 60,
   login_action_button: 40,
-  logout_action_button: 40
+  logout_action_button: 40,
 };
 
 export const fontcolor = {
-  account_component_instructions: 'rgba(84,90,92,1.00)',
-  account_component_greetings: 'rgba(84,90,92,1.00)',
-  login_action_button: 'rgba(84,90,92,1.00)',
-  logout_action_button: 'rgba(84,90,92,1.00)'
+  account_component_instructions: "rgba(84,90,92,1.00)",
+  account_component_greetings: "rgba(84,90,92,1.00)",
+  login_action_button: "rgba(84,90,92,1.00)",
+  logout_action_button: "rgba(84,90,92,1.00)",
 };
