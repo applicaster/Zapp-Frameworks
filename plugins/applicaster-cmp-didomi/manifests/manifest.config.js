d8052143b94de935b874cd266fa1396b00c2084f
Alex <24409942+inckie@users.noreply.github.com>
2021-05-04 09:49:22 -0400

    split manifest

--- plugins/applicaster-cmp-didomi/manifests/manifest.config.js
+++ plugins/applicaster-cmp-didomi/manifests/manifest.config.js
@@ -95,7 +95,7 @@ function createManifest({ version, platform }) {
   return manifest;
 }
 
-const custom_configuration_fields_apple = [
+const custom_configuration_fields = [
   {
     key: "present_on_startup",
     type: "checkbox",
@@ -110,16 +110,18 @@ const custom_configuration_fields_apple = [
     label: "API key",
     default: "",
     tooltip_text: "API key",
-  },
-  {
+  }
+];
+
+const custom_configuration_fields_apple = custom_configuration_fields.concat(
+  [{
     key: "ios_assets_bundle",
     type: "uploader",
     label: "Logo drawables zip",
     label_tooltip: "Please upload a zip file to provide the logo assets for this plugin. File name must match json override in the Didomi web console"
-  }
-];
+  }]);
 
-const custom_configuration_fields_android = custom_configuration_fields_apple.concat(
+const custom_configuration_fields_android = custom_configuration_fields.concat(
     [{
       "key": "android_assets_bundle",
       "type": "uploader",
