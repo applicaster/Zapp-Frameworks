586ecfc7a8ab7284514afb996e0a7a23f0b62006
kononenkoAnton <a.kononenko@applicaster.com>
2021-05-06 15:00:37 -0400

    change local storage to session storage

--- plugins/first-time-user-expirience-screen/src/Services/LocalStorageService/index.ts
+++ plugins/first-time-user-expirience-screen/src/Services/LocalStorageService/index.ts
@@ -1,10 +1,11 @@
 import { localStorage } from "@applicaster/zapp-react-native-bridge/ZappStorage/LocalStorage";
+import { sessionStorage } from "@applicaster/zapp-react-native-bridge/ZappStorage/SessionStorage";
 
 const localStorageNamespace = "first-time-user-expirience-screen";
 const screenWasPresentedKey = "screenWasPresented";
 
 export async function getBuildNumber() {
-  return await localStorage.getItem("version_name");
+  return await sessionStorage.getItem("version_name");
 }
 
 export async function localStorageSet(versionName) {
