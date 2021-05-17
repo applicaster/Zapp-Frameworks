e1dd1b79085c2453e1867eb66dcb5acabd292c29
sherwino <s.garcia@applicaster.com>
2021-05-17 19:01:17 -0400

    fix: adding dimensions hook to theoplayer view

--- plugins/zapp-react-native-theo-player/src/THEOplayerView.js
+++ plugins/zapp-react-native-theo-player/src/THEOplayerView.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import {requireNativeComponent} from 'react-native';
+import React from "react";
+import { requireNativeComponent, View, StyleSheet } from "react-native";
+import { useDimensions } from "@applicaster/zapp-react-native-utils/reactHooks";
 
-const THEOplayerViewNative = requireNativeComponent('THEOplayerView', THEOplayerView);
+const THEOplayerViewNative = requireNativeComponent(
+  "THEOplayerView",
+  THEOplayerView
+);
 
-export default class THEOplayerView extends React.Component {
-    render() {
-        return <THEOplayerViewNative
-            {...this.props}
-        />;
-    }
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#000" },
+  player: {
+    width: "100%",
+    height: "100%",
+    padding: 0,
+    margin: 0,
+  },
+});
+
+export default function THEOplayerView(props) {
+  const window = useDimensions("window");
+
+  return (
+    <View style={styles.container}>
+      <THEOplayerViewNative
+        {...props}
+        style={{ ...styles.player, width: window?.width }}
+      />
+    </View>
+  );
 }
