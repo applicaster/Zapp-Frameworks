742d2f985c65a258b195e3e4bf28ef3646910162
Alex Zchut <a.zchut@applicaster.com>
2021-05-25 08:24:57 +0300

    register default topics if not in a list of registered

--- plugins/zapp-push-plugin-firebase/apple/ios/APPushProviderFirebase.swift
+++ plugins/zapp-push-plugin-firebase/apple/ios/APPushProviderFirebase.swift
@@ -46,6 +46,13 @@ open class APPushProviderFirebase: ZPPushProvider {
         return topics
     }()
     
+    lazy var defaultTopicsToRegister: [String] = {
+        var topics:[String] = defaultTopics
+        topics.append(contentsOf: localizedDefaultTopics)
+        
+        return topics.unique()
+    }()
+    
     lazy var languageCode: String? = {
         return FacadeConnector.connector?.storage?.sessionStorageValue(for: "languageCode", namespace: nil)
     }()
@@ -175,11 +182,7 @@ open class APPushProviderFirebase: ZPPushProvider {
             // topics already defined, no changes needed, update local array
             registeredTopics = Set(topics)
         } else {
-            // add default value
-            var topics:[String] = defaultTopics
-            topics.append(contentsOf: localizedDefaultTopics)
-
-            addTagsToDevice(topics.unique()) { _, _ in
+            addTagsToDevice(defaultTopicsToRegister) { _, _ in
                 // do nothing
             }
         }
@@ -190,8 +193,6 @@ open class APPushProviderFirebase: ZPPushProvider {
         registeredTopics.removeAll()
         // clean local storage topics
         updateTopicsInLocalStorage()
-        // set default
-        setDefaultTopicIfNeeded()
     }
 
     fileprivate var namespace: String? {
@@ -233,8 +234,9 @@ extension APPushProviderFirebase: MessagingDelegate {
         // clean local registered topics
         cleanRegisteredTopics()
 
-        // subscribe to topics appears in local storage
-        addTagsToDevice(registeredTopicsInLocalStorage, completion: { _, _ in
+        // subscribe to topics appears in local storage, add default if not there
+        let topicsToRegister = registeredTopicsInLocalStorage ?? [] + defaultTopicsToRegister
+        addTagsToDevice(topicsToRegister.unique(), completion: { _, _ in
             // do nothing
         })
     }
