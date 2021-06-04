af78ceb729e8aec81da550670b7784e4a1cabce7
Alex <24409942+inckie@users.noreply.github.com>
2021-06-04 15:40:07 -0400

    feat: bump xray to 0.2.1

--- plugins/quick-brick-xray/manifests/manifest.config.js
+++ plugins/quick-brick-xray/manifests/manifest.config.js
@@ -201,7 +201,7 @@ function extra_npm_dependencies(platform) {
     platform === "android_tv_for_quickbrick" ||
     platform === "amazon_fire_tv_for_quickbrick"
   ) {
-    return ["@applicaster/x-ray@0.1.7"];
+    return ["@applicaster/x-ray@0.2.1"];
   }
   return [];
 }
