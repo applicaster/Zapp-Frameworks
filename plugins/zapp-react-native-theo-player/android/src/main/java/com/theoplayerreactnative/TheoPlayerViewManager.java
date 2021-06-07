d1ee870875275e7d7d966d371b2b8dcc79870d5f
Alex <24409942+inckie@users.noreply.github.com>
2021-06-07 16:52:27 -0400

    revert fullscreen hacks

--- plugins/zapp-react-native-theo-player/android/src/main/java/com/theoplayerreactnative/TheoPlayerViewManager.java
+++ plugins/zapp-react-native-theo-player/android/src/main/java/com/theoplayerreactnative/TheoPlayerViewManager.java
@@ -1,7 +1,6 @@
 package com.theoplayerreactnative;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.text.TextUtils;
 import android.view.View;
@@ -286,18 +285,6 @@ public class TheoPlayerViewManager extends SimpleViewManager<THEOplayerView> imp
 
     private void goFullscreen(Activity currentActivity) {
         final Window window = currentActivity.getWindow();
-        playerView.postDelayed(()->{
-            playerView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }, 5000);
-
         playerView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
