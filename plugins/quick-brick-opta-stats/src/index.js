20bc40d531dbdbfb86fd9454bda43895a1b3ba93
Sherwino <s.garcia@applicaster.com>
2021-06-09 17:56:52 -0400

    fix: adding check to run screen init once

--- plugins/quick-brick-opta-stats/src/index.js
+++ plugins/quick-brick-opta-stats/src/index.js
@@ -1,20 +1,19 @@
 import * as React from "react";
-import { NativeModules } from "react-native";
+import { NativeModules, View } from "react-native";
 
 import { createLogger } from "./logger";
 import { DEFAULT } from "./utils";
 import { styles } from "./styles";
 
-import {requireNativeComponent} from 'react-native';
+import { requireNativeComponent } from "react-native";
 
 import { useNavigation } from "@applicaster/zapp-react-native-utils/reactHooks/navigation";
 
-
-const OptaStatsContainer = requireNativeComponent('OptaStatsContainer');
+const OptaStatsContainer = requireNativeComponent("OptaStatsContainer");
 
 const logger = createLogger();
 
-openScreen = async(url, complete) => {
+openScreen = async (url, complete) => {
   const packageName = DEFAULT.packageName;
   const methodName = DEFAULT.methodName;
   const screenPackage = NativeModules?.[packageName];
@@ -39,52 +38,62 @@ openScreen = async(url, complete) => {
   }
 
   if (!method) {
-    logger.error(`Method ${methodName} is not found in the package ${packageName}`);
+    logger.error(
+      `Method ${methodName} is not found in the package ${packageName}`
+    );
     complete();
     return;
-  };
+  }
 
   try {
-    const res = await method({url});
+    const res = await method({ url });
     logger.info(`Received response from native method ${methodName}`, res);
   } catch (error) {
-    logger.error(`Method ${methodName} the package ${packageName} failed with error ${error}`);
+    logger.error(
+      `Method ${methodName} the package ${packageName} failed with error ${error}`
+    );
   }
   complete();
 };
 
 useUrlSchemeHandler = async ({ query, url, onFinish }) => {
-
   const complete = () => {
     onFinish((done) => {
       done();
     });
-  }
+  };
   openScreen(url, complete);
-
 };
 
 export default StatScreens = (params: Any) => {
   const url = params?.screenData?.link?.href;
-  if(url) {
-    logger.info({message: "COPA url", data: {url}});
+
+  if (url) {
+    logger.info({ message: "COPA url", data: { url } });
     const navigator = useNavigation();
     const onDismiss = () => {
       if (navigator.canGoBack()) {
         logger.info("Dismissed native screen, going back");
         navigator.goBack();
       } else {
-        logger.warn("Dismissed native screen, can't go back, trying to go home");
+        logger.warn(
+          "Dismissed native screen, can't go back, trying to go home"
+        );
         navigator.goHome();
       }
     };
-    openScreen(url, onDismiss);
-    //return <View style={styles.container}></View>; //fake screen does not work
+
+    React.useEffect(() => {
+      url && openScreen(url, onDismiss);
+    }, []);
+
+    return <View style={styles.container} />;
   }
-  return <OptaStatsContainer style={styles.container}></OptaStatsContainer>
+
+  return <OptaStatsContainer style={styles.container}></OptaStatsContainer>;
 };
 
 StatScreens.urlScheme = {
   host: "copa_stats",
   handler: useUrlSchemeHandler,
-}
+};
