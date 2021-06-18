ae347561683f5ac0227012281adbbbef881ac47b
kononenkoAnton <a.kononenko@applicaster.com>
2021-06-18 15:18:30 -0400

    Add support theo player support to disable buffer (#356)

--- plugins/zapp-react-native-theo-player/src/index.tsx
+++ plugins/zapp-react-native-theo-player/src/index.tsx
@@ -126,6 +126,7 @@ export default class THEOPlayer extends Component<Props, State> {
 
   constructor(props) {
     super(props);
+    this.disableBufferAnimation = this.disableBufferAnimation.bind(this);
     this.getCurrentTime = this.getCurrentTime.bind(this);
     this.hadwareBackButtonDidPressed = this.hadwareBackButtonDidPressed.bind(
       this
@@ -490,6 +491,10 @@ export default class THEOPlayer extends Component<Props, State> {
   _assignRoot = (component: React.Component<any, any, any>) => {
     this.playerRef = component;
   };
+
+  disableBufferAnimation(): boolean {
+    return true;
+  }
 
   handleEnded() {
     logger.debug({
