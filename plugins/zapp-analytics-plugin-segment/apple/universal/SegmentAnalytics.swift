258d07166fe29f4e99c12e33a6b530c8543be94e
Alex Zchut <a.zchut@applicaster.com>
2021-06-29 13:58:28 +0300

    update for segment analytics save/restore login data

--- plugins/zapp-analytics-plugin-segment/apple/universal/SegmentAnalytics.swift
+++ plugins/zapp-analytics-plugin-segment/apple/universal/SegmentAnalytics.swift
@@ -26,7 +26,13 @@ class SegmentAnalytics: NSObject, PluginAdapterProtocol {
     }
 
     public func getKey() -> String {
-        return "SegmentAnalytics"
+        return Params.pluginKey
+    }
+
+    struct Params {
+        static let pluginKey = "SegmentAnalytics"
+        static let pluginIdentifier = "segment_analytics"
+        static let identityStorageKey = "identity"
     }
 
     var isDisabled = false
@@ -34,7 +40,7 @@ class SegmentAnalytics: NSObject, PluginAdapterProtocol {
     public var playerPlugin: PlayerProtocol?
     var objcHelper: SegmentAnalyticsHelper?
     var playerRateObserverPointerString: UInt?
-    
+
     lazy var ignoredEvents: [String] = {
         guard let eventsListString = model?.configurationValue(for: "blacklisted_events_list") as? String,
               eventsListString.isEmpty == false else {
@@ -71,6 +77,8 @@ class SegmentAnalytics: NSObject, PluginAdapterProtocol {
             SEGAnalytics.setup(with: configuration)
             objcHelper = SegmentAnalyticsHelper(providerProperties: providerProperties,
                                                 delegate: self)
+
+            sendLoginInformationIfAvailable()
             completion?(true)
         } else {
             disable(completion: completion)
@@ -103,14 +111,119 @@ extension SegmentAnalytics {
     @objc public func login(with identity: String,
                             traits: [String: Any]?,
                             options: [String: Any]?) {
-
-        //check if the plugin is enabled 
+        // check if the plugin is enabled
         guard isDisabled == false else {
+            storeLoginData(with: identity,
+                           traits: traits,
+                           options: options)
             return
-        }       
+        }
 
         SEGAnalytics.shared()?.identify(identity,
                                         traits: traits,
                                         options: options)
+    }
+
+    func storeLoginData(with identity: String,
+                        traits: [String: Any]?,
+                        options: [String: Any]?) {
+        let object = IdentityObject(identity: identity,
+                                    traits: traits,
+                                    options: options)
+
+        DispatchQueue.main.async {
+            _ = FacadeConnector.connector?.storage?.sessionStorageSetValue(for: Params.identityStorageKey,
+                                                                           value: object.toJsonString(),
+                                                                           namespace: Params.pluginIdentifier)
+        }
+    }
+
+    func fetchLoginData() -> IdentityObject? {
+        guard let storageValue = FacadeConnector.connector?.storage?.sessionStorageValue(for: Params.identityStorageKey,
+
+                                                                                         namespace: Params.pluginIdentifier) else {
+            return nil
+        }
+
+        // remove the content
+        _ = FacadeConnector.connector?.storage?.sessionStorageRemoveValue(for: Params.identityStorageKey,
+                                                                          namespace: Params.pluginIdentifier)
+        return IdentityObject(jsonString: storageValue)
+    }
+
+    func sendLoginInformationIfAvailable() {
+        guard let identityObject = fetchLoginData() else {
+            return
+        }
+
+        login(with: identityObject.identity,
+              traits: identityObject.traits,
+              options: identityObject.options)
+    }
+}
+
+struct IdentityObject {
+    var identity: String
+    var traits: [String: Any]?
+    var options: [String: Any]?
+
+    private enum CodingKeys: String {
+        case identity, traits, options
+    }
+
+    init(identity: String,
+         traits: [String: Any]?,
+         options: [String: Any]?) {
+        self.identity = identity
+        self.traits = traits
+        self.options = options
+    }
+
+    init(jsonString: String) {
+        let object = IdentityObject.fromJsonString(jsonString)
+        identity = object?.identity ?? ""
+        traits = object?.traits
+        options = object?.options
+    }
+
+    init(dictionary: [String: Any]?) {
+        identity = dictionary?[CodingKeys.identity.rawValue] as? String ?? ""
+        traits = dictionary?[CodingKeys.traits.rawValue] as? [String: Any]
+        options = dictionary?[CodingKeys.options.rawValue] as? [String: Any]
+    }
+
+    private var dictionary: [String: Any] {
+        return [
+            CodingKeys.identity.rawValue: identity,
+            CodingKeys.traits.rawValue: traits ?? [:],
+            CodingKeys.options.rawValue: options ?? [:],
+        ]
+    }
+
+    func toJsonString(_ options: JSONSerialization.WritingOptions = .prettyPrinted) -> String? {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: dictionary,
+                                                      options: options)
+            let jsonString = String(data: jsonData,
+                                    encoding: .utf8)
+            return jsonString
+        } catch {
+            return nil
+        }
+    }
+
+    static func fromJsonString(_ jsonString: String) -> IdentityObject? {
+        var retValue: IdentityObject?
+        guard let data = jsonString.data(using: String.Encoding.utf8) else {
+            return retValue
+        }
+
+        do {
+            let dictionary = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]
+            retValue = IdentityObject(dictionary: dictionary)
+        } catch {
+            print("unable to parse the identity object")
+        }
+        return retValue
     }
 }
